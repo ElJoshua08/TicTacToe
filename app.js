585c1871128cb46a52582da24e04fe876a45bb68
Joshua <josuealejandrof926@gmail.com>
2023-08-04 21:02:43 +0200

    Finished light theme

--- app.js
+++ app.js
@@ -1,129 +1,135 @@
+// Elements
+const body = document.body;
+const theme_toggle = document.getElementById("theme-btn");
+
+const game_container = document.querySelector(".game-container");
+game_container.classList.add("active");
+const turn = document.querySelector(".turn");
+turn.classList.add("active");
+
+const win_menu = document.querySelector(".win-menu");
+const win_text = document.querySelector(".title");
+const replay_btn = document.querySelector(".win-btn");
+
+const cells = document.querySelectorAll(".cell");
+const letter = document.querySelector(".letter");
+
 // Variables
-const game_container = document.querySelector('.game-container')
-game_container.classList.add('active')
-const turn = document.querySelector('.turn')
-turn.classList.add('active')
-
-const win_menu = document.querySelector('.win-menu')
-const win_text = document.querySelector('h1')
-const replay_btn = document.querySelector('.win-btn')
-
-const cells = document.querySelectorAll('.cell')
-const letter = document.querySelector('h2')
-
-const X = 'fa-x'
-const O = 'fa-o'
+const X = "fa-x";
+const O = "fa-o";
 
 const wining_combos = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 let xTurn = true;
-let activeCellsArray = Array(9).fill(null)
+let activeCellsArray = Array(9).fill(null);
 
-replay_btn.addEventListener('click', restart)
+// Restart on replay btn click
+replay_btn.addEventListener("click", restart);
 
 function restart() {
-    activeCellsArray = Array(9).fill(null)
-    cells.forEach((cell) => {
-        cell.classList.remove('active')
-        cell.classList.remove('static')
-        cell.firstChild.classList.remove(X)
-        cell.firstChild.classList.remove(O)
+  activeCellsArray = Array(9).fill(null);
+  cells.forEach((cell) => {
+    cell.classList.remove("active");
+    cell.classList.remove("static");
+    cell.firstChild.classList.remove(X);
+    cell.firstChild.classList.remove(O);
+  });
 
-    })
-    win_menu.classList.remove('active')
-    game_container.classList.add('active')
-    turn.classList.add('active')
-    xTurn = true;
+  turn.style.opacity = 1;
+  game_container.style.opacity = 1;
 
-    letter.innerHTML = 'X'
-    letter.style.textShadow = '0 0 5px rgb(255, 255, 0)'
-    letter.style.color = 'rgb(255, 255, 0)'
+  win_menu.classList.remove('active')
+
+  xTurn = true;
+  letter.innerHTML = "X";
+
+  console.log('xq ahora no vas')
 }
 
 function onStart() {
-    gameLogic()
+  gameLogic();
 }
 
 function gameLogic() {
-    cells.forEach((cell, index) => {
-        cell.addEventListener('click', function() {
-            if(!cell.classList.contains('static')) {
-                if(xTurn) {
-                    cell.firstChild.classList.add(X)
-                    cell.firstChild.style.color = 'rgb(255, 255, 0)'
-                    cell.firstChild.style.textShadow = '0 0 5px rgb(255, 255, 0)'
-                    cell.classList.add('static')
-                    
-                } else {
-                    cell.firstChild.classList.add(O)
-                    cell.firstChild.style.color = 'rgb(0, 255, 255)'
-                    cell.firstChild.style.textShadow = '0 0 5px rgb(0, 255, 255)'
-                    cell.classList.add('static')
-                }
+  cells.forEach((cell, index) => {
+    cell.addEventListener("click", function () {
+      if (!cell.classList.contains("static")) {
+        if (xTurn) {
+          cell.firstChild.classList.add(X);
+          cell.classList.add("static");
 
-                cell.classList.add('active')
+          letter.classList.add(O);
+          letter.classList.remove(X);
+        } else {
+          cell.firstChild.classList.add(O);
+          cell.classList.add("static");
 
-                activeCellsArray.splice(index, 1, cell.firstChild.classList.item(1))
+          letter.classList.add(X);
+          letter.classList.remove(O);
+        }
 
-                letter.innerHTML = xTurn == true ? 'O' : 'X'
-                letter.style.textShadow = xTurn == true ? '0 0 5px rgb(255, 255, 0)' : '0 0 5px rgb(0, 255, 255)'
-                letter.style.color = xTurn == true ? 'rgb(0, 255, 255)' : 'rgb(255, 255, 0)' 
-                xTurn = !xTurn
-            }
-            let winPlayer = checkWin() ? checkWin() : null
-            if(winPlayer) {
-                game_container.classList.remove('active')
-                turn.classList.remove('active')
+        cell.classList.add("active");
 
-                win_text.innerHTML = winPlayer
+        activeCellsArray.splice(index, 1, cell.firstChild.classList.item(1));
 
-                if(winPlayer == 'X has won!') {
-                    win_text.style.color = 'yellow'
-                    win_text.style.textShadow = '0 0 5px yellow'
-                } else if(winPlayer == 'O has won!') {
-                    win_text.style.color = 'cyan'
-                    win_text.style.textShadow = '0 0 5px cyan'
-                } else {
-                    win_text.style.color = 'white'
-                    win_text.style.textShadow = '0 0 5px white'
-                }
+        letter.innerHTML = xTurn == true ? "O" : "X";
 
-                win_menu.classList.add('active')
-            } 
-            else if(!activeCellsArray.includes(null)) {
-                game_container.classList.remove('active')
-                turn.classList.remove('active')
+        xTurn = !xTurn;
+      }
+      let winPlayer = checkWin() ? checkWin() : null;
+      if (winPlayer) {
+        game_container.style.opacity = 0.1;
+        turn.style.opacity = 0.1;
 
-                win_text.style.color = 'white'
-                win_text.style.textShadow = '0 0 5px white'
-                
-                win_text.innerHTML = 'Draw'
-                win_menu.classList.add('active')
-            }
-        })
-    })
+        win_text.innerHTML = winPlayer;
+        win_menu.classList.add("active");
+      } else if (!activeCellsArray.includes(null)) {
+        turn.style.opacity = 0.1;
+        game_container.style.opacity = 0.1;
+
+        win_text.classList.add("draw");
+
+        win_text.innerHTML = "Draw";
+        win_menu.classList.add("active");
+      }
+    });
+  });
 }
 
 function checkWin() {
-    for (const [a, b, c] of wining_combos) {
-        if(activeCellsArray[a] != null) {
-            let letter = activeCellsArray[a]
+  for (const [a, b, c] of wining_combos) {
+    if (activeCellsArray[a] != null) {
+      let letter = activeCellsArray[a];
 
-            if(activeCellsArray[b] == letter && activeCellsArray[c] == letter) {
-                return letter == 'fa-x' ? 'X has won!' : 'O has won!'
-            }
-        }
+      if (activeCellsArray[b] == letter && activeCellsArray[c] == letter) {
+        return letter == "fa-x" ? "X has won!" : "O has won!";
+      }
     }
-    return false
+  }
+  return false;
 }
 
-onStart()
+// Change theme on btn click
+theme_toggle.onclick = (e) => {
+  theme_toggle.classList.toggle("light");
+  game_container.classList.toggle("light");
+  win_menu.classList.toggle("light");
+  turn.classList.toggle("light");
+  body.classList.toggle("light");
+  letter.classList.toggle("light");
+};
+
+onStart();
+
+body.onclick = () => {
+  console.log(letter);
+};
